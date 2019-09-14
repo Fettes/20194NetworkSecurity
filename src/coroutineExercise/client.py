d56becff2b107f9b839b8d72bf3b1f6b5f0c6204
Fettes <396511673@qq.com>
2019-09-13 21:18:24 -0400

    x

--- src/coroutineExercise/client.py
+++ src/coroutineExercise/client.py
@@ -21,7 +21,7 @@ class EchoClientProtocol(asyncio.Protocol):
         if self.flag <= len(command_list) - 1:
             command = self.send_message(command_list[self.flag])
             self.transport.write(command.encode())
-            if respond == "You can't reach it up there!" or respond == "It's too low to hit.":
+            if respond == "You can't reach it up there!" or respond == "It's too low to hit." or respond == "You can't hit that!":
                 self.flag = self.flag
             else:
                 self.flag = self.flag + 1
