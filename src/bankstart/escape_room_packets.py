e5902abbbbd5048da9272455c88406da489f850e
Tianshi Feng <tianshifeng@Tianshis-MacBook-Air.local>
2019-09-29 19:54:01 -0400

    x

--- src/bankstart/escape_room_packets.py
+++ src/bankstart/escape_room_packets.py
@@ -1,5 +1,5 @@
 from playground.network.packet import PacketType
-from playground.network.packet.fieldtypes import STRING
+from playground.network.packet.fieldtypes import STRING, UNIT32, BUFFER
 
 
 class GameCommandPacket(PacketType):
@@ -41,23 +41,71 @@ class GameResponsePacket(PacketType):
         return self.res
 
 
+class GameInitRequestPacket(PacketType):
+    DEFINITION_IDENTIFIER = "20194.exercise7.gameinit"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+        ("username_string", STRING),
+    ]
+
+
+class GamePaymentRequestPacket(PacketType):
+    DEFINITION_IDENTIFIER = "20194.exercise7.gamepaymentrequest"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+        ("unique_id", STRING),
+        ("account", STRING),
+        ("amount", UNIT32)
+    ]
+
+
+class GamePaymentResponsePacket(PacketType):
+    DEFINITION_IDENTIFIER = "20194.exercise7.gamepaymentresponse"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+        ("receipt", BUFFER),
+        ("receipt_sig", BUFFER),
+    ]
+
+
 def create_game_init_packet(username):
-    class game_init_packet(PacketType):
-        DEFINITION_IDENTIFIER = "20194.exercise6.game_init_packet"
-        DEFINITION_VERSION = "1.0"
-
-        FIELDS = [
-            ("usrname", STRING),
-        ]
-
-    tmp = game_init_packet()
-    tmp.usrname = username
-    return game_init_packet
+    return GameInitRequestPacket(username_string=username)
 
 
 def process_game_init(pkt):
-    print(pkt)
     return "tianshi"
 
 
-# def create_game_require_pay_packet():
+def create_game_require_pay_packet(unique_id, account, amount):
+    return GamePaymentRequestPacket(unique_id=unique_id, account=account, amount=amount)
+
+
+def process_game_require_pay_packet(pkt):
+    return pkt.unique_id, pkt.account, pkt.amount
+
+
+def create_game_pay_packet(receipt, receipt_signature):
+    return GamePaymentResponsePacket(receipt=receipt, receipt_sig=receipt_signature)
+
+
+def process_game_pay_packet(pkt):
+    return pkt.receipt, pkt.receipt_sig
+
+
+def create_game_response(response, status):
+    return GameResponsePacket(res=response, sta=status)
+
+
+def process_game_response(pkt):
+    return pkt.res
+
+
+def create_game_command(command):
+    return GameCommandPacket(command_line=command)
+
+
+def process_game_command(pkt):
+    return pkt.command_line
